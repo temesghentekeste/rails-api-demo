13538cd141af6bf0d6891c8e75c58141a6b98698
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-21 21:00:10 +0300

    Migrate db

--- db/migrate/20210621174807_create_books.rb
+++ db/migrate/20210621174807_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.title :author
+      t.string :author
 
       t.timestamps
     end
