74c904dbacc365569e7badd185164f105cedbb24
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-22 15:19:58 +0300

    Install factory bot rails and update index action test

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 describe 'Books API', type: :request do
     it 'returns all books' do 
+        FactoryBot.create(:book, title: '1984', author: 'George Orwell')
+        FactoryBot.create(:book, title: 'Current times', author: 'Temesghen')
+        
         get '/api/v1/books'
 
         expect(response).to have_http_status(:success)
+
+        expect(JSON.parse(response.body).size).to eq(2)
     end
 
 end
