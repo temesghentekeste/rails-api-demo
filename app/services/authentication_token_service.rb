a8302a1ac5c92fabe23b6b43697dbed571d2c65a
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-24 00:16:52 +0300

    Authentication Service

---
+++ app/services/authentication_token_service.rb
@@ -0,0 +1,11 @@
+class AuthenticationTokenService
+    
+    HMAC_SECRET = 'my$ecretK3y'
+    ALGORITHM_TYPE = 'HS256'
+
+    def self.call 
+        payload = { 'data' => 'blah'}
+
+        JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
+    end
+end
